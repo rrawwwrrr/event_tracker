99e9d7c345e275608c74b32900db88b8e627738a
rrr <ruslan.ramazanov@antara-services.ru>
2023-10-16 11:21:49 +0400

    init

--- version.php
+++ version.php
@@ -1,10 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = 2021030400;
-$plugin->version  = 2021030400;
+$plugin->version  = 2021030401;
 $plugin->requires = 2016052300;
 $plugin->component = 'local_event_tracker';
 
-$plugin->release = '1.0.4'; 
+$plugin->release = '1.0.5'; 
 $plugin->maturity = MATURITY_STABLE;
